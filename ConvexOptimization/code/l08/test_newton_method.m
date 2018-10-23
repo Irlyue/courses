ada3bf1c103337ce0dcb7c25c12e476c4b47eafe
Wenfeng <1554484645@qq.com>
2018-10-23 11:06:30 +0800

    changes for test code of newton method

--- ConvexOptimization/code/l08/test_newton_method.m
+++ ConvexOptimization/code/l08/test_newton_method.m
@@ -2,10 +2,14 @@ clear;
 x = zeros(2, 1);
 [x, his] = newton_method(x, @func_l08, 20);
 
-x = zeros(2, 1);
-A = [[1, 3]; [1, -3];, [-1, 0]];
 
+% Use CVX package
+xt = zeros(2, 1);
+A = [[1, 3]; [1, -3];, [-1, 0]];
 cvx_begin
-variable x(2);
-minimize(sum(exp(A*x)));
+variable xt(2);
+minimize(sum(exp(A*xt)));
 cvx_end
+
+disp(['xt  = [', sprintf('%8.4f ', xt), ']', '(CVX)']);
+disp(['x   = [', sprintf('%8.4f ', x), ']', '(mine)']);
